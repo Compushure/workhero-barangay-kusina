fdb854535e6be6c9c927aa590a33e7cbdb3f4aa1
jpross14 <rosalitajp04@gmail.com>
2026-02-02 00:22:22 +0800

    Refactor/manager assignment DEV-208 DEV-132 DEV-122 DEV-211 DEV-124 DEV-125 DEV-229 DEV-225 DEV-235 (#87)
    * feat: added remarks upon action of verification for declining or accepting request
    
    * refactor: edited ui for more smooth effects
    
    * refactor: edited the ui of task assigning part for smooth ui/ux
    
    * build: updated the dependencies from next 16.0.10 to 16.1.6
    
    * fix: fixed conflicts
    
    * refactor: edited the buttons ui
    
    * refactor: edited buttons
    
    * refactor: edited ui
    
    * fix: applied global font and fixed task-view pagination
    
    * feat: added number display of task-view cards and employee-view cards; additional ui polishing
    
    * refactor: ui polishing for view cards  and other task-assignment component ui
    
    * feat: status patches in both employee badges in task-view and in task abdges in employee-view
    
    * feat: separated the assigned to section of task-view card to its own modular component (TaskViewEmployeeBadges)
    
    * refactor: TaskViewEmployeeBadges status patch now use a more modular implementation
    
    * refactor: fixed edit task dialog ui so all its contents fit the screen without scrolling
    
    * refactor: ui polish for edit task dialog, select task dialog and other ui components in task assignment page
    
    * refactor: removed redundant buttons on task assignment card modals, added a overdue badge in task-view card
    
    * feat: added updateTaskPoints functionality to select-task-dialog
    
    * fix: task points update immediately in the UI without requiring a page refresh
    
    * refactor: renamed taskType to taskDescription because the task's description is what is really being passed through that variable
    
    * feat: added skeleton component in ui loading states for view-cards, select-task table and assign-employees table
    
    * fix: skeleton hydration error; assignment-card buttons are cleared/disabled if selected task has all employees assigned
    
    * fix: removed unnecessary horizontal scrolling in task-assignment page
    
    ---------
    
    Co-authored-by: Ulimer25 <165236341+Ulimer25@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import {Roboto} from 'next/font/google';
+
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export default function Home() {
   return (
