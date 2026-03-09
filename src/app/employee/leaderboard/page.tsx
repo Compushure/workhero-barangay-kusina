212b66431b8d57f13d480e8d98cc65e7db41e990
Ulimer25 <165236341+Ulimer25@users.noreply.github.com>
2026-03-10 07:08:23 +0800

    CHORE DEV-401 Update the MetaData of the Webpage and add icon (#156)

--- src/app/employee/leaderboard/page.tsx
+++ src/app/employee/leaderboard/page.tsx
@@ -1,6 +1,16 @@
 import { protectEmployeeRoute } from '@/actions/shared/auth';
 import { getLatestLeaderboardPeriods } from '@/actions/hr/leaderboard';
 import { LeaderboardPageClient } from '@/components/employee/leaderboard/leaderboard-page';
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface';
+
+export const metadata: Metadata = {
+  title: 'WorkHero | Leaderboard',
+  icons: {
+    icon: '/assets/website-logo.svg',
+    shortcut: '/assets/website-logo.svg',
+    apple: '/assets/website-logo.svg',
+  },
+};
 
 export default async function LeaderboardPage() {
   await protectEmployeeRoute();
