744d169a8cde81b4bebfbdc948e184d3d8f23b5c
Ulimer25 <165236341+Ulimer25@users.noreply.github.com>
2026-03-09 15:31:47 +0800

    Refactor: edited also the metadata for both employee and admin login pages.

--- src/app/auth/adminlogin/page.tsx
+++ src/app/auth/adminlogin/page.tsx
@@ -6,8 +6,18 @@
  */
 
 import { Suspense } from 'react';
+import type { Metadata } from 'next';
 import { SuperadminLoginContainer } from '@/components/auth/login/superadmin/superadmin-login-container';
 import { redirectifSessionExists } from '@/actions/shared/auth';
+
+export const metadata: Metadata = {
+  title: 'WorkHero | Admin Login',
+  icons: {
+    icon: '/assets/website-logo.svg',
+    shortcut: '/assets/website-logo.svg',
+    apple: '/assets/website-logo.svg',
+  },
+};
 
 export function LoadingFallback() {
   return (
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,7 +1,17 @@
 import { Suspense } from 'react';
+import type { Metadata } from 'next';
 import { EmployeeLoginContainer } from '@/components/auth/login/employee/employee-login-container';
 import { LoadingFallback } from '@/app/auth/adminlogin/page';
 import { redirectifSessionExists } from '@/actions/shared/auth';
+
+export const metadata: Metadata = {
+  title: 'WorkHero | Login',
+  icons: {
+    icon: '/assets/website-logo.svg',
+    shortcut: '/assets/website-logo.svg',
+    apple: '/assets/website-logo.svg',
+  },
+};
 export default async function LoginPage() {
   await redirectifSessionExists();
 
