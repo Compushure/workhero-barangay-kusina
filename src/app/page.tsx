f8d2385425f9ddef907f67e709f6dc366d56bf5a
Ulimer25 <165236341+Ulimer25@users.noreply.github.com>
2026-03-14 23:44:45 +0800

    Refactor: put metadata for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Search } from 'lucide-react';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'WorkHero | Home',
+  icons: {
+    icon: '/assets/website-logo.svg',
+    shortcut: '/assets/website-logo.svg',
+    apple: '/assets/website-logo.svg',
+  },
+};
 
 export default function Home() {
   return (
