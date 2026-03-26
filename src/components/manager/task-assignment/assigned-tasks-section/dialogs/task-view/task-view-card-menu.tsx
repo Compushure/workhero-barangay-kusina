b795c9a0fd1f4a264f2ca5d9b684c981fc3bed01
jpross14 <dallyeoartemis@gmail.com>
2026-03-26 15:35:47 +0800

    fix: adjust z-index for EmployeeViewCardMenu popover and button

--- src/components/manager/task-assignment/assigned-tasks-section/dialogs/task-view/task-view-card-menu.tsx
+++ src/components/manager/task-assignment/assigned-tasks-section/dialogs/task-view/task-view-card-menu.tsx
@@ -27,7 +27,7 @@ export default function TaskViewCardMenu({
           <MoreVertical className="size-5" />
         </button>
       </PopoverTrigger>
-      <PopoverContent className="w-32 p-1.5 bg-card" align="end">
+      <PopoverContent portalled={false} className="w-32 p-1.5 bg-card" align="end">
         <div className="flex flex-col gap-0.5">
           {isOverdue ? (
             <Tooltip>
