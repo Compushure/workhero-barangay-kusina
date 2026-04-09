7e3f32ab52f6af3baf71c54ff632a92dc042a002
A. Legayada <tonilegayada@gmail.com>
2026-04-09 20:06:28 +0800

    test: Update user e2e tests: waits + CRUD & searches
    cypress/e2e/superadmin/users.cy.ts: increase long wait intervals and adjust waits around add-user flow to reduce flakiness, and add full user CRUD coverage and search scenarios. New tests cover editing a user, deleting a user, and searching by role (manager, hr, regular). Removed placeholder commented tests and added assertions for success messages and visibility checks.

--- cypress/e2e/superadmin/users.cy.ts
+++ cypress/e2e/superadmin/users.cy.ts
@@ -1,8 +1,8 @@
 describe('When the superadmin logs in', () => {
   const waitIntervalShort = 200
   const waitInterval = 500 // Adjust lng
-  const waitIntervalLong = 1500
-  const waitIntervalExtraLong = 2500
+  const waitIntervalLong = 1800
+  const waitIntervalExtraLong = 3000
   const testSuperAdminUser = {
     name: 'Cypress Superadmin',
     email: 'cypress.superadmin@example.com',
@@ -81,26 +81,68 @@ describe('When the superadmin logs in', () => {
     // reference 
     const addButton =cy.get('button[aria-label="Add User"]')
     addButton.click()
-    cy.wait(waitIntervalLong)
+    cy.wait(waitIntervalExtraLong)
     cy.get('#add-name').type(testUsertoAdd.name)
     cy.get('#add-email').type(testUsertoAdd.email)
     cy.get('#add-password').type(testUsertoAdd.password)
     cy.get('#add-contact').type(testUsertoAdd.contactNumber)
     cy.get('button[type="submit"][form="add-user-form"]').click()
+    cy.wait(waitIntervalLong)
     cy.wait(waitIntervalExtraLong).contains(testUsertoAdd.name).should('be.visible')
     cy.wait(waitIntervalExtraLong).contains(/success/i).should('be.visible')
-
-
-
-
   })
+
   it('should allow super admin to edit a user', () => {
-    cy.get('p[title="Cypress User To Add"]')
-     cy.wait(waitInterval)
+    cy.get('p[title="Cypress User To Add"]').click()
+    cy.wait(waitInterval)
+    cy.contains('button', 'Edit User').click()
+    cy.wait(waitIntervalExtraLong)
+    cy.contains('button', 'Edit All').click()
+    cy.wait(waitIntervalShort)
+    cy.get('#edit-name').clear().type(editedTestUser.name)
+    cy.get('#edit-contact').clear().type(editedTestUser.contactNumber)
+    cy.get('#edit-address').clear().type(editedTestUser.address)
+    cy.get('#edit-tin').clear().type(editedTestUser.tin)
+    cy.get('#edit-sss').clear().type(editedTestUser.sss)
+    cy.get('#edit-pagibig').clear().type(editedTestUser.pagibig)
+    cy.get('button[type="submit"][form="edit-user-form"]').click()
+    cy.wait(waitIntervalLong)
+    cy.wait(waitIntervalExtraLong).contains(editedTestUser.name).should('be.visible')
+    cy.contains(/success/i).should('be.visible')
 
   })
-  // it('should allow super admin to delete a user', () => {})
-  // it('should allow a user to search for a manager user', () => {})
-  // it('should allow a user to search for a hr user', () => {})
-  // it('should allow a user to search for a regular user', () => {})
+
+  it('should allow super admin to delete a user', () => {
+    cy.get('p[title="Cypress User Edited"]').click()
+    cy.wait(waitInterval)
+    cy.contains('button', 'Delete User').click()
+    cy.wait(waitIntervalExtraLong)
+    cy.get('button[data-variant="destructive"][data-size="default"]').click()
+    cy.wait(waitIntervalExtraLong).contains(editedTestUser.name).not('be.visible')
+    cy.wait(waitIntervalLong).contains(/success/i).should('be.visible')
+
+  })
+  it('should allow a user to search for a manager user', () => {
+    // Open the combobox
+    cy.get('#filter-type').click()
+    cy.contains('[role="option"]', /manager/i).click()
+    cy.get('#search').type(testManagerUser.name)
+    cy.wait(waitIntervalLong).contains(testManagerUser.name).should('be.visible')
+
+
+  })
+  it('should allow a user to search for a hr user', () => {
+    cy.get('#filter-type').click()
+    cy.contains('[role="option"]', /hr/i).click()
+    cy.get('#search').type(testHRUser.name)
+    cy.wait(waitIntervalLong).contains(testHRUser.name).should('be.visible')
+
+  })
+
+  it('should allow a user to search for a regular user', () => {
+    cy.get('#filter-type').click()
+    cy.contains('[role="option"]', /regular/i).click()
+    cy.get('#search').type(testRegularUser.name)
+    cy.wait(waitIntervalLong).contains(testRegularUser.name).should('be.visible')
+  })
 })
